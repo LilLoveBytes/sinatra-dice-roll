124322f633dd34e2c11b496c94216027152cdc6f
La Tasha L.  Pollard <128096377+LilLoveBytes@users.noreply.github.com>
2025-01-28 02:45:04 +0000

    loops and list rolls

--- dice.rb
+++ dice.rb
@@ -11,44 +11,53 @@ BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do 
   # erb argument must be symbol of views file name
-  erb(:elephant)
+  erb(:home)
 end
 
 get("/dice/2/6") do
-  num_1 = rand(1..6)
-  num_2 = rand(1..6)
-  summ = num_1 + num_2
-
-  @msg = "You rolled a #{num_1.to_i} and a #{num_2.to_i} for a total of #{summ}"
+  @rolls = []
+  2.times do 
+    num = rand(1..6)
+    @rolls.push(num)
+  end 
 
   erb(:two_six)
 end 
 
 get("/dice/2/10") do
-  num_1 = rand(1..10)
-  num_2 = rand(1..10)
-  summ = num_1 + num_2
-  
-  @msg = "You rolled a #{num_1.to_i} and a #{num_2.to_i} for a total of #{summ}"
-
+  @rolls = []
+  2.times do
+    num = rand(1..10)
+    @rolls.push(num)
+  end 
   erb(:two_ten)
-  end
+end
 
 get("/dice/1/20") do
-  num_1 = rand(1..20)
-  @msg = "You rolled a #{num_1}"
+  @rolls = [] 
+  num = rand(1..20)
+  @rolls.push(num)
 
   erb(:one_twenty)
 end
 
 get("/dice/5/4") do
-  num_1 = rand(1..4)
-  num_2 = rand(1..4)
-  num_3 = rand(1..4)
-  num_4 = rand(1..4)
-  num_5 = rand(1..4)
-  summ = num_1 + num_2 + num_3 + num_4 + num_5
-  @msg = "You rolled a #{num_1}, a #{num_2}, a #{num_3}, a #{num_4}, and a #{num_5} for a total of #{summ}"
+  @rolls = []
+  5.times do
+    num = rand(1..4)
+    @rolls.push(num)
+  end
 
   erb(:five_four)
 end
+
+get("/dice/100/6") do
+  @rolls = []
+
+  100.times do
+    num = rand(1..6)
+    @rolls.push(num)
+  end 
+
+  erb(:one_hundred_six)
+end 
