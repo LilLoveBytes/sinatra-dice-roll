65a4fb146550449f4dafe2c402cf5564b4ca0ade
La Tasha L.  Pollard <128096377+LilLoveBytes@users.noreply.github.com>
2025-01-28 01:27:11 +0000

    updates erb for each route

--- dice.rb
+++ dice.rb
@@ -19,10 +19,9 @@ get("/dice/2/6") do
   num_2 = rand(1..6)
   summ = num_1 + num_2
 
-  msg = "You rolled a #{num_1.to_i} and a #{num_2.to_i} for a total of #{summ}"
+  @msg = "You rolled a #{num_1.to_i} and a #{num_2.to_i} for a total of #{summ}"
 
-  " <h1> 2 6-sided dice </h1>
-  <p> #{msg} </p> "
+  erb(:two_six)
 end 
 
 get("/dice/2/10") do
@@ -30,17 +29,16 @@ get("/dice/2/10") do
   num_2 = rand(1..10)
   summ = num_1 + num_2
   
-  msg = "You rolled a #{num_1.to_i} and a #{num_2.to_i} for a total of #{summ}"
+  @msg = "You rolled a #{num_1.to_i} and a #{num_2.to_i} for a total of #{summ}"
 
-  " <h1> 2 10-sided dice</h1>
-  <p> #{msg} </p>"
+  erb(:two_ten)
   end
 
 get("/dice/1/20") do
   num_1 = rand(1..20)
-  msg = "You rolled a #{num_1}"
-  "<h1> A 20-sided die </h1>
-  <p> #{msg} </p>"
+  @msg = "You rolled a #{num_1}"
+
+  erb(:one_twenty)
 end
 
 get("/dice/5/4") do
@@ -50,7 +48,7 @@ get("/dice/5/4") do
   num_4 = rand(1..4)
   num_5 = rand(1..4)
   summ = num_1 + num_2 + num_3 + num_4 + num_5
-  msg = "You rolled a #{num_1}, a #{num_2}, a #{num_3}, a #{num_4}, and a #{num_5} for a total of #{summ}"
-  "<h1> 5 4-sided dice</h1>
-  <p> #{msg} </p>"
+  @msg = "You rolled a #{num_1}, a #{num_2}, a #{num_3}, a #{num_4}, and a #{num_5} for a total of #{summ}"
+
+  erb(:five_four)
 end
